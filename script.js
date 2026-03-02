bace431153d4812e317725143d927529b5526c6b
eg987 <egamundson@gmail.com>
2026-03-01 18:06:29 -0600

    Hamburge menu accessiility and focus changes

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // opens/closes hamburger menu when clicked
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  
+  const isOpen = menu.classList.toggle("open");
+  icon.classList.toggle("open");
+  
+  //  tells screen readers the current state of menu open/closed
+  icon.setAttribute("aria-expanded", isOpen);
 }
